ae87743a1fa38e3afdcec112dabda611e93fb5d4
Morten Johansson <morten1.kodehode@gmail.com>
2022-09-13 14:05:26 +0200

    added likes functionality

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Script
+const like = document.getElementById("like");
+const likes = document.getElementById("likeNr");
+
+let addLikes = 0;
+
+function addLike() {
+  addLikes++;
+  likes.innerHTML = addLikes;
+}
